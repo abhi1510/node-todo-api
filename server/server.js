15cbacb7b715c8e60e90e4f8e36745ea5585d0d9
Abhinav Kumar <abhinav.kumar@axisrooms.com>
2018-05-02 23:28:06 +0530

    added get todo by id

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {ObjectID} = require('mongodb');
 const bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
@@ -25,6 +26,21 @@ app.post('/todos', (req, res) => {
         res.send(doc);
     }, (err) => {
         res.status(400).send(err);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();    
+        }
+        res.send({todo});
+    }).catch((err) => {
+        res.status(400).send();    
     });
 });
 
